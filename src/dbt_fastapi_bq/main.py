bf542de5e9a633eb8e40ced3cf2a0ac6acff8ce5
Lorcan Rae <lorcanrae@gmail.com>
2025-07-28 19:45:13 +0300

    renamed exception Dbt command exception

--- src/dbt_fastapi_bq/main.py
+++ src/dbt_fastapi_bq/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
 
-from dbt_fastapi_bq.routes import dbt
-from dbt_fastapi_bq.exceptions import DbtRunException
+from dbt_fastapi_bq.routes import api_router
+from dbt_fastapi_bq.exceptions import DbtCommandException
 
 app = FastAPI(
     title="dbt FastAPI Wrapper",
@@ -17,11 +17,11 @@ def root():
     return {"status": "running"}
 
 
-app.include_router(dbt.router, prefix="/dbt", tags=["dbt"])
+app.include_router(api_router)
 
 
-@app.exception_handler(DbtRunException)
-async def dbt_run_exception_handler(request: Request, exc: DbtRunException):
+@app.exception_handler(DbtCommandException)
+async def dbt_run_exception_handler(request: Request, exc: DbtCommandException):
     validated = exc.detail.model_dump()
     return JSONResponse(
         status_code=exc.status_code,
