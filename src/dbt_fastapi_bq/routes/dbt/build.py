d6f033a992a3b63a61746e168a22a5b8db887456
Lorcan Rae <lorcanrae@gmail.com>
2025-07-31 13:18:21 +0300

    updated attribute call and schemas in build, run, test endpoint scripts

--- src/dbt_fastapi_bq/routes/dbt/build.py
+++ src/dbt_fastapi_bq/routes/dbt/build.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from dbt_fastapi_bq.dbt_manager import DbtManager
 
 from dbt_fastapi_bq.schemas.dbt_schema import (
-    DbtBuildRequest,
+    DbtBuildListRequest,
     DbtCommandResponse,
 )
 
@@ -18,11 +18,11 @@ COMMAND = "build"
     summary=f"Execute 'dbt {COMMAND}'",
 )
 async def run_dbt(
-    payload: DbtBuildRequest,
+    payload: DbtBuildListRequest,
 ) -> DbtCommandResponse:
     dbt_manager = DbtManager(verb=COMMAND, **payload.model_dump())
 
     output = dbt_manager.execute_dbt_command()
-    metadata = {"dbt_command": " ".join(dbt_manager.dbt_cmd)}
+    metadata = {"dbt_command": " ".join(dbt_manager.dbt_cli_command)}
 
     return DbtCommandResponse(status="success", output=output, metadata=metadata)
--- src/dbt_fastapi_bq/routes/dbt/test.py
+++ src/dbt_fastapi_bq/routes/dbt/test.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from dbt_fastapi_bq.dbt_manager import DbtManager
 
 from dbt_fastapi_bq.schemas.dbt_schema import (
-    DbtRunTestRequest,
+    DbtRunTestCompileSeedSnapshotDocs,
     DbtCommandResponse,
 )
 
@@ -18,11 +18,11 @@ COMMAND = "test"
     summary=f"Execute 'dbt {COMMAND}'",
 )
 async def run_dbt(
-    payload: DbtRunTestRequest,
+    payload: DbtRunTestCompileSeedSnapshotDocs,
 ) -> DbtCommandResponse:
     dbt_manager = DbtManager(verb=COMMAND, **payload.model_dump())
 
     output = dbt_manager.execute_dbt_command()
-    metadata = {"dbt_command": " ".join(dbt_manager.dbt_cmd)}
+    metadata = {"dbt_command": " ".join(dbt_manager.dbt_cli_command)}
 
     return DbtCommandResponse(status="success", output=output, metadata=metadata)
