230ae96ccb042e050971efd130d1a682a0c7dca4
Lorcan Rae <lorcanrae@gmail.com>
2025-11-03 18:13:54 +0200

    main.py: added exception handlers

--- src/dbt_fastapi/main.py
+++ src/dbt_fastapi/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 
 from dbt_fastapi.routes import dbt_router
+from dbt_fastapi.exceptions import DbtFastApiError
+from dbt_fastapi.exception_handlers import dbt_error_handler, generic_exception_handler
 
 
 app = FastAPI(
     title="dbt FastAPI Wrapper",
     version="0.3.0",
-    description="Exposes dbt CLI over HTTP",
+    description="dbt over HTTP",
 )
+
+app.add_exception_handler(DbtFastApiError, dbt_error_handler)
+app.add_exception_handler(Exception, generic_exception_handler)
 
 
 @app.get("/")
