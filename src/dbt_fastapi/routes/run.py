ea69c8d6fd04f34fd1d218ad9e5542fa7f397f2c
Lorcan Rae <lorcanrae@gmail.com>
2025-11-05 19:22:58 +0200

    routes: integrated new request/response schema

--- src/dbt_fastapi/routes/run.py
+++ src/dbt_fastapi/routes/run.py
@@ -2,13 +2,12 @@ from fastapi import APIRouter, Depends
 
 from dbt_fastapi.dbt_manager import DbtManager
 from dbt_fastapi.config import DbtConfig, get_dbt_config
-from dbt_fastapi.schemas.dbt_schema import (
-    DbtRunTestCompileSeedSnapshotDocsRequest,
-    DbtResponse,
-)
+from dbt_fastapi.schemas.request_schema import DbtRunRequest
+from dbt_fastapi.schemas.response_schema import DbtRunResponse, DbtMetadataBase
 
 
 router = APIRouter()
+
 
 COMMAND = "run"
 
@@ -16,11 +15,12 @@ COMMAND = "run"
 @router.post(
     f"/{COMMAND}",
     summary=f"Execute 'dbt {COMMAND}'",
+    response_model=DbtRunResponse,
 )
 def run_dbt(
-    payload: DbtRunTestCompileSeedSnapshotDocsRequest,
+    payload: DbtRunRequest,
     config: DbtConfig = Depends(get_dbt_config),
-) -> DbtResponse:
+) -> DbtRunResponse:
     dbt_manager = DbtManager(
         verb=COMMAND,
         target=payload.target or config.dbt_target_default,
@@ -37,12 +37,16 @@ def run_dbt(
     # Extract list of nodes
     nodes = dbt_manager.get_nodes_from_result(result)
 
-    metadata = {
-        "command": COMMAND,
-        "dbt_command": " ".join(dbt_manager.dbt_cli_args),
-        "target": dbt_manager.target,
-        "nodes_processed": len(nodes),
-        "selection_criteria": dbt_manager.get_selection_criteria_string(),
-    }
+    metadata = DbtMetadataBase(
+        command=COMMAND,
+        dbt_command=" ".join(dbt_manager.dbt_cli_args),
+        target=dbt_manager.target,
+        nodes_processed=len(nodes),
+        selection_criteria=dbt_manager.get_selection_criteria_string(),
+    )
 
-    return DbtResponse(success=result.success, nodes=nodes, metadata=metadata)
+    return DbtRunResponse(
+        success=result.success,
+        nodes=nodes,
+        metadata=metadata,
+    )
