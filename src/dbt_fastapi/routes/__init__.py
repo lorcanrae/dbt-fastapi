6bd1319c86e58c3d81d3850883c080c8d995410c
Lorcan Rae <lorcanrae@gmail.com>
2025-11-06 12:15:27 +0200

    added compile to dbt router

--- src/dbt_fastapi/routes/__init__.py
+++ src/dbt_fastapi/routes/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from . import run, test, build, list
+from . import run, test, build, list, compile
 
 dbt_router = APIRouter(prefix="/dbt", tags=["dbt"])
 
@@ -7,3 +7,4 @@ dbt_router.include_router(run.router)
 dbt_router.include_router(test.router)
 dbt_router.include_router(build.router)
 dbt_router.include_router(list.router)
+dbt_router.include_router(compile.router)
